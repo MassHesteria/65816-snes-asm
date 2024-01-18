b08dd5aa0e67e3413ea897ba5ca39863849d4c42
MassHesteria <brad.hester@gmail.com>
2024-01-18 07:32:25 -0600

    prep for hover game support

--- src/hover.ts
+++ src/hover.ts
@@ -35,10 +35,32 @@ export const opcodeHoverProvider: vscode.HoverProvider = {
     const h = docs.find(p => p.code == word);
     if (h != undefined) {
       hoverText = h.docs;
+    } else {
+      const config = vscode.workspace.getConfiguration("65816Snes");
+      const gameSupport = config.get<string>("gameSupport", "none");
+      if (gameSupport == "Super Metroid") {
+        hoverText = getSuperMetroidHoverText(document, position);
+      }
     }
 
     // Create a Hover object with the defined content
     const hover = new vscode.Hover(new vscode.MarkdownString(hoverText));
     return hover;
   },
+};
+
+const getSuperMetroidHoverText = (
+  document: vscode.TextDocument,
+  position: vscode.Position
+): string => {
+  const wordRange = document.getWordRangeAtPosition(position);
+  const word = wordRange ? document.getText(wordRange) : "";
+  let hoverText = "";
+  if (word == "$09A6") {
+    hoverText = "Equipped Items"
+  }
+  if (word == "09A6") {
+    hoverText = "test Equipped Items"
+  }
+  return hoverText;
 };
